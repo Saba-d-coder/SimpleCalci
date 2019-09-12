04c0b5070f7daf63585a1d13ac22c5143f03f4e8
Saba Khan <saba100.ksk@gmail.com>
2019-09-12 22:09:40 +0530

    Added button.css fle

--- src/components/button.js
+++ src/components/button.js
@@ -1,19 +1,9 @@
 import React from 'react';
-
-// css styles object which will be passed inline to input tag
-const styles={
-    height:"40px",
-    width:"67px",
-    backgroundColor:"black",
-    fontSize:"19px",
-    border:"1px solid grey",
-    boxShadow:"2px 2px 5px rgb(30,30,30)",
-    color:"white"
-};
+import '../button.css';
 
 const Button =(props) =>{
     return(
-        <input style={styles} type="button" value={props.value} onClick={props.handleClick}/>
+        <input className="Btn" type="button" value={props.value} onClick={props.handleClick}></input>
     );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import Calci from './components/calci';
 import * as serviceWorker from './serviceWorker';
 
